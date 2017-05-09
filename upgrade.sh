09f8ec5a2e2f4488dab41d1f210b257aa4612f03
coscale_pvhertum <peter.vanhertum@coscale.com>
2017-05-09 11:57:59 +0200

    script to upgrade parts or all

--- upgrade.sh
+++ upgrade.sh
@@ -2,12 +2,18 @@
 
 SERVICE=$1
 
-if [ "$SERVICE" == "agent-builder" ]; then
+if [ "$SERVICE" == "" ]; then
+  echo "Usage : $0 [<SERVICE>/all/coscale/data]"
+elif [ "$SERVICE" == "agent-builder" ]; then
   echo "upgrading agent-builder"
   ./stop.sh haproxy && ./stop.sh api && ./run.sh api && ./connect.sh api /opt/coscale/agent-builder/update.sh && ./stop.sh api && ./run.sh api && ./run.sh haproxy
+elif [ "$SERVICE" == "all" ]; then
+  ./stop.sh && ./run.sh
+elif [ "$SERVICE" == "coscale" ]; then
+  ./stop.sh coscale && ./run.sh coscale
+elif [ "$SERVICE" == "data" ]; then
+  ./stop.sh data && ./run.sh data
 else
   echo "upgrading service"
   ./stop.sh $1 && ./run.sh $1
 fi
-
-
