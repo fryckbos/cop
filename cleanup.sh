3a37fd3b1cf901cb7aa30342d7f20982794fd1c7
bartdevylder <bartdevylder@gmail.com>
2017-05-09 16:07:07 +0200

    adding sleep option to clean script

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,12 @@ source conf.sh
 
 SLACK=1
 KEEP=30
+SLEEP=60
 
 if [ "$1" == "list" ]; then
   ./connect.sh cron java -cp /opt/coscale/cron/bin/cron-${VERSION}-jar-with-dependencies.jar coscale.cron.CassandraCleanerWorker -r rabbitmq --slack ${SLACK} --keep ${KEEP} list
 elif [ "$1" == "delete" ]; then
-  ./connect.sh cron java -cp /opt/coscale/cron/bin/cron-${VERSION}-jar-with-dependencies.jar coscale.cron.CassandraCleanerWorker -r rabbitmq --slack ${SLACK} --keep ${KEEP} delete
+  ./connect.sh cron java -cp /opt/coscale/cron/bin/cron-${VERSION}-jar-with-dependencies.jar coscale.cron.CassandraCleanerWorker -r rabbitmq --slack ${SLACK} --keep ${KEEP} --sleep ${SLEEP} delete
 else
   echo "usage: $0 <list|delete>"
 fi
