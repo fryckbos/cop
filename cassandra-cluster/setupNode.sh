79b16159747e3a812bdd3f51d4116590b509806f
fryckbos <frederick.ryckbosch@gmail.com>
2017-07-07 15:52:07 +0200

    Add cassandra cluster setup scripts.

---
+++ cassandra-cluster/setupNode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+if [ $# -lt 1 ]; then
+    echo "Usage: `basename $0` seeds" 1>&2
+    exit 255
+fi
+
+source ../conf.sh
+
+docker run -d \
+    -e CASSANDRA_IS_SEED=false \
+    -e CASSANDRA_SEED_ADDRESS=$1 \
+    -v `pwd`/../data/cassandra:/var/lib/cassandra \
+    --net=host \
+    --restart unless-stopped \
+    --name coscale_cassandra_node $REGISTRY/coscale/cassandra:$VERSION
