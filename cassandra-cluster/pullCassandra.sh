93c6ae1252f518d5f49224a8aa4eb70915c184d5
fryckbos <frederick.ryckbosch@gmail.com>
2017-07-07 17:44:29 +0200

    Add script to pull Cassandra.

---
+++ cassandra-cluster/pullCassandra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+source ../conf.sh
+
+docker login -u "$REGISTRY_USERNAME" -p "$REGISTRY_PASSWORD" -e "$REGISTRY_EMAIL" $REGISTRY
+
+function pull {
+    SERVICE=$1
+    IMAGE_VERSION=$2
+    IMG=coscale/$SERVICE:$IMAGE_VERSION
+    docker pull $REGISTRY/$IMG
+    docker tag $REGISTRY/$IMG $IMG
+}
+
+pull cassandra $VERSION
