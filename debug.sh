1b2389be7efc5bdd7745139b56f4de1b347ec9db
Samuel Vandamme <samuel@sava.be>
2017-07-26 11:11:31 +0200

    Added debug script for gathering information about the environment and running containers

---
+++ debug.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+
+source conf.sh
+source services.sh
+
+NAME=${1:-all}
+
+if [ "$NAME" == "--help" ]; then
+    echo "$0 : debug all services."
+    echo "$0 <service> : debug a specific service."
+    echo "$0 coscale : debug the CoScale services."
+    exit 0
+fi
+
+# Create main debug directory if it doesn't exist
+mkdir -p debug/
+
+# Create current debug directory
+DATE=$(date +%Y_%m_%d_%H%M%S)
+DIR="debug/$DATE"
+mkdir -p "$DIR"
+
+function debug {
+    DIR=$1
+    SERVICE=$2
+
+    # Create directory for service
+    mkdir -p "$DIR/$SERVICE"
+
+    # Output internal logs to dir
+    ./connect $SERVICE log > "$DIR/$SERVICE/log_internal"
+
+    # Output docker logs to dir
+    docker logs coscale_$SERVICE > "$DIR/$SERVICE/log_docker"
+
+    # Output docker inspect to dir
+    docker inspect coscale_$SERVICE > "$DIR/$SERVICE/inspect"
+}
+
+# Stop the data services
+for SERVICE in $DATA_SERVICES; do
+    if [ "$NAME" == "all" ] || [ "$NAME" == "data" ] || [ "$NAME" == "$SERVICE" ]; then
+        debug $DIR $SERVICE
+    fi
+done
+
+# Stop the coscale services
+for SERVICE in $COSCALE_SERVICES $LB_SERVICE; do
+    if [ "$NAME" == "all" ] || [ "$NAME" == "coscale" ] || [ "$NAME" == "$SERVICE" ]; then
+        debug $DIR $SERVICE
+    fi
+done
+
+# Output current location to see on which disk we are
+pwd > "$DIR/pwd"
+
+# Output disk space available
+df -h > "$DIR/df"
+
+# Output memory available
+free -m > "$DIR/free"
+
+# Output version 
+echo $VERSION > "$DIR/version"
+
+# Tar the directory and remove 
+tar -zcvf "$DATE.tar.gz" "$DIR"
+rm -rf "$DIR"
