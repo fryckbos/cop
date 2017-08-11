dd240fc3bf18266ac323afb0186563ecd53e9f47
coscale_pvhertum <peter.vanhertum@coscale.com>
2017-08-11 12:12:35 +0200

    COD-10112 add upload script

---
+++ upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source conf.sh
+
+file=$1
+PID=$$
+echo "Starting upload of file [${file}] with username [${REGISTRY_USERNAME}]"
+echo "---------------------------"
+curl -o /tmp/upload-${PID} -X POST -H "Content-Type: multipart/form-data" -F "fname=@${file}" -F "scripted=1" https://${REGISTRY_USERNAME}:${REGISTRY_PASSWORD}@opdebug.coscale.com:8443/
+echo "---------------------------"
+cat /tmp/upload-${PID}
+echo "---------------------------"
+rm /tmp/upload-${PID}
+echo "Done."
+
