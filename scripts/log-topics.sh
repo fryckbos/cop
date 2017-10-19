541699f17d49694cead0878bcdefbe8cc6edaf58
root <root@coscale.c2rdev210xjedmftdfdc4pm55h.ax.internal.cloudapp.net>
2017-10-19 13:14:15 +0000

    fix logtopics script

--- scripts/log-topics.sh
+++ scripts/log-topics.sh
@@ -1,1 +1,1 @@
-./connect.sh streamingroller java -cp /opt/coscale/streamingroller/bin/streamingroller-1.0.9143-jar-with-dependencies.jar coscale.streamingroller.kafka.LogTopics -config /opt/coscale/streamingroller/etc/streamingroller.conf
+./connect.sh streamingroller java -cp /opt/coscale/streamingroller/bin/streamingroller-1.0.9143-jar-with-dependencies.jar coscale.streamingcore.kafka.LogTopics -config /opt/coscale/streamingroller/etc/streamingroller.conf
