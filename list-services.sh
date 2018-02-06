3d5dee1632552e3ba8973b5dc6023a6ca54f7725
fryckbos <frederick.ryckbosch@gmail.com>
2018-02-06 12:02:24 +0100

    Moved list-services into diagnose.sh

--- list-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-source conf.sh
-source services.sh
-
-echo "DATA_SERVICES=$DATA_SERVICES"
-echo "COSCALE_SERVICES=$COSCALE_SERVICES"
-echo "DEPRECATED_SERVICES=$DEPRECATED_SERVICES"
